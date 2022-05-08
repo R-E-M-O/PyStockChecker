5f0a2a2877fe0366a7eed15b03e04c49ff9f33eb
Omer H <73497880+R-E-M-O@users.noreply.github.com>
2022-05-08 07:30:53 -0400

    Implemented requestHTML, writeHTML, partial inStock and partial runStockCheck

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import requests
 def requestHTML(url):
     # html request
     request = requests.get(url, timeout=5)
-    htmlContent = str(request.content)
+
+    # return the contents
+    return request.content
+
+def writeHTML(htmlContent):
 
     # write the html code to a txt document
     htmlFile = open("htmlContent.txt", "w")
@@ -15,27 +19,48 @@ def requestHTML(url):
     htmlFile.close()
 
     # return the contents
-    return htmlContent
+    return str(htmlContent)
 
 
-def runStockCheck():
+def inStock(htmlString):
+    inStock = False
+
+    if "" is in htmlString:
+        inStock = True
+    # body yet to be complete
+    return inStock
+
+
+def runStockCheck(url):
     # body yet to be complete
 
+    # request new html contents from url and write to file
+    htmlContent = requestHTML(url)
+    htmlString = writeHTML(htmlContent)
+
+    # check for inStock
+    inStock = inStock(htmlString)
+
+    # keep checking stock until item is in stock
+    while inStock is False:
+        htmlContent = requestHTML(url)
+        htmlString = writeHTML(htmlContent)
+        inStock = inStock(htmlString)
+
+    #next print inStockMessage to txt file and console
     return
 
 
-def inStock():
-    inStock = False
 
-    # body yet to be complete
-
-    return inStock
 
 
 def main():
     # body yet to be complete
 
-    url = input("Hello World!")
+    # prompt for url and save html request content to string
+    url = input("Enter a url: ")
+
+    runStockCheck(url)
 
 
 main()
